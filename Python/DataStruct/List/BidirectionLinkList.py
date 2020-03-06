98a1baaa099b9332607bf49d3dd71dff2517b1cb
Xiaohai.lin <tbfungeek@163.com>
2020-03-06 11:02:46 +0800

    [+]添加双向循环列表接口

--- Python/DataStruct/List/BidirectionLinkList.py
+++ Python/DataStruct/List/BidirectionLinkList.py
@@ -1,8 +1,63 @@
 #-*-coding:utf-8-*-
 #!/usr/bin/env python3
 
-class BidirectionLinkList(object):
+class Node(object):
     pass
+
+class BidirectionLinkList(object):
+
+    def __init__(self):
+        pass
+
+    def create_node(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def get(self,index):
+        pass
+
+    def first(self):
+        pass
+
+    def last(self):
+        pass
+
+    def insert_value_to_head(self,value):
+        pass
+
+    def insert_value_before(self,value,new_value):
+        pass
+
+    def insert_value_after(self,value,new_value):
+        pass
+
+    def insert_value_to_tail(self,value):
+        pass
+
+    def delete_head_node(self):
+        pass
+
+    def delete_value_before(self,value):
+        pass
+
+    def delete_value_after(self,value):
+        pass
+
+    def delete_tail_value(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    
 
 if __name__ == "__main__":
     pass
