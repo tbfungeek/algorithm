2627983506e402a0b2f11d010b6fb03745432645
Xiaohai.lin <tbfungeek@163.com>
2020-03-06 19:38:52 +0800

    [+]queue implements

--- Python/DataStruct/Queue/Queue.py
+++ Python/DataStruct/Queue/Queue.py
@@ -23,29 +23,73 @@ class Node(object):
     def __str__(self):
         return str(self.__data)
 
-
 class Queue(object):
     
     def __init__(self):
         self.__head = None
         self.__tail = None
+        self.__count = 0
 
     def enqueue(self,value):
-        pass
+        node = Node(value)
+        if self.__head is None and self.__tail is None:
+            self.__head = self.__tail = node
+        else:
+            self.__tail.next = node
+            self.__tail = node
+        self.__count += 1
 
     def dequeue(self):
-        pass
+
+        if self.__head is None:
+            return None
+        #only one element
+        if self.__tail == self.__head:
+            node = self.__head
+            self.__tail = self.__head = None
+            self.__count -= 1
+            return node
+        
+        node = self.__head
+        self.__head = self.__head.next
+        self.__count -= 1
+        return node
 
     def __str__(self):
-        pass
+        value = []
+        for item in self:
+            value.append(str(item.data))
+        return " ====> ".join(value)
 
     def __iter__(self):
-        pass
-    
+        if self.__head is None:
+            return
+        current_node = self.__head
+        while current_node != self.__tail:
+            yield current_node
+            current_node = current_node.next
+        yield self.__tail
+
     def size(self):
-        pass
+        return self.__count
 
 if __name__ == '__main__':
-    pass
+
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+
+    print(queue)
+
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
 
     
