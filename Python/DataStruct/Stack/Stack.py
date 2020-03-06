95bee6e47581289d3030ffd5ecf6d1480afff44c
Xiaohai.lin <tbfungeek@163.com>
2020-03-06 18:23:28 +0800

    [+] 堆栈实现

--- Python/DataStruct/Stack/Stack.py
+++ Python/DataStruct/Stack/Stack.py
@@ -27,19 +27,68 @@ class Stack(object):
     
     def __init__(self):
         self.__top = None
+        self.__count = 0
 
     def push(self,value):
-        pass
+        if value is None:
+            return
+        node = Node(value)
+        #add the node to top of top place
+        node.next = self.__top
+        #point the top point to node
+        self.__top = node
+        self.__count += 1
 
     def pop(self):
-        pass
+        if self.__top == None:
+            return
+        top_value = self.__top.data
+        self.__top = self.__top.next
+        return top_value
+        self.__count -= 1
 
     def top(self):
-        pass
+        return self.__top
 
     def size(self):
-        pass
+        return self.__count
+        
+    def __str__(self):
+        value = []
+        for item in self:
+            value.append(str(item.data))
+        return " ===> ".join(value)
+
+    def __iter__(self):
+        current_item = self.__top
+        while current_item != None:
+            yield current_item
+            current_item = current_item.next
 
 if __name__ == '__main__':
-    pass
+
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+    stack.push(6)
+    stack.push(7)
+    print(stack)
+
+    print(stack.top())
+    print(stack.size())
+    
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    print(stack)
     
