297602fba2124cb92a2e0227862703d7c2d39dd0
Xiaohai.lin <tbfungeek@163.com>
2020-04-26 09:25:41 +0800

    [*]提交临时代码

--- Python/DataStruct/Graphic/MST/Prim.py
+++ Python/DataStruct/Graphic/MST/Prim.py
@@ -1,3 +1,4 @@
+import math 
 class Vex(object):
 
     def __init__(self,value):
@@ -7,7 +8,10 @@ class Vex(object):
     
     def connected_vexs(self):
         return self.connected_vex.keys()
-    
+
+    def connected_vexs_values(self):
+        return self.connected_vex.values()
+
     def weight_of_connected_vex(self,vex):
         if not self.is_connected_vex():
             return -1
@@ -30,19 +34,43 @@ class Adjacency_Table_Graphic(object):
 
     def __init__(self):
         self.vexs = {}
+        self.edge_num = 0
+    
+    def vex_num(self):
+        return len(self.vexs.keys())
 
     def add_vex(self,value):
 
         if value not in self.vexs.keys():
-            vex = Vex(value)
-            self.vexs[value] = vex
+            self.vexs[value] = Vex(value)
 
         return self.vexs[value]
 
     def add_edge(self,start,end,weight):
         start_vex = self.add_vex(start)
         end_vex = self.add_vex(end)
+        if start_vex.is_connected_vex(end):
+            return
         start_vex.connect_to(end,weight)
+        self.edge_num += 1
+
+    def prim(self, start_point):
+        path = []
+        if len(self.vexs.keys()) <= 0 or self.edge_num < self.vex_num() - 1:
+            return path
+        path = []
+        #将起点放到已经选择的数组中
+        selected_vexs = [start_point]
+        #将全部点都添加为候选点
+        candidate_vexs = self.vexs.keys()[:]
+
+        while len(candidate_vexs) > 0:
+            for vex in selected_vexs:
+                connected_vexs = vex.connected_vex()
+                for connected_vex in connected_vexs:
+                    if connected_vex.value not in candidate_vexs:
+                        continue
+                    min_weight = 999999999999
 
 
 if __name__ == "__main__":
@@ -57,22 +85,9 @@ if __name__ == "__main__":
 
     g.add_edge(0,1,7)
     g.add_edge(0,5,5)
-
-    g.add_edge(1,0,7)
     g.add_edge(1,4,3)
     g.add_edge(1,2,9)
-
-    g.add_edge(2,1,9)
     g.add_edge(2,3,6)
-
-    g.add_edge(3,2,6)
     g.add_edge(3,4,8)
     g.add_edge(3,5,10)
-
-    g.add_edge(4,3,8)
-    g.add_edge(4,1,3)
     g.add_edge(4,5,4)
-
-    g.add_edge(5,0,5)
-    g.add_edge(5,3,10)
-    g.add_edge(5,4,4)
