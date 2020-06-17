4ae6f494eea10b20f8021751e2890efa60654d1d
Xiaohai.lin <tbfungeek@163.com>
2020-06-17 08:49:27 +0800

    two sum

--- Python/LeedCode/1_Two_Sum.py
+++ Python/LeedCode/1_Two_Sum.py
@@ -1,11 +1,23 @@
-# https://leetcode-cn.com/problems/two-sum/
+def twoSum_01(nums,target):
+    sorted_id = sorted(range(len(nums)), key=lambda k: nums[k])
+    head = 0
+    tail = len(nums) - 1
+    result = nums[sorted_id[head]] + nums[sorted_id[tail]]
+    while result != target:
+        if result < target and head <= len(nums) - 1:
+            head += 1
+        elif result > target and tail >= 1 :
+            tail -= 1
+        result = nums[sorted_id[head]] + nums[sorted_id[tail]]
+    return [sorted_id[head], sorted_id[tail]]
 
-def two_sum(nums, target):
-    for i in range(0,len(nums) - 1):
-            for j in range(1,len(nums)):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i,j]
-    
-if __name__ == '__main__':
-    test_list = [2, 7, 11, 15]
-    print(two_sum(test_list,9))
+def twoSum_02(nums, target):
+
+    hashset = {} #key -> nums[index] value -> index
+    for i in range(len(nums)):
+        temp = target - nums[i]
+        if temp in hashset:
+            return [hashset[temp],i]
+        hashset[nums[i]] = i
+            
+print(twoSum_02([3,2,4],6))
