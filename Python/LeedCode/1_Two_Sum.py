8d5787d36748e7e3976a08e7bcffe848b1bbc0f9
Xiaohai.lin <tbfungeek@163.com>
2020-06-17 08:57:23 +0800

    two sum

--- Python/LeedCode/1_Two_Sum.py
+++ Python/LeedCode/1_Two_Sum.py
@@ -1,3 +1,26 @@
+'''
+
+【题目】
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+【更多题解见】：https://leetcode-cn.com/submissions/detail/79586807/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+'''
+
 def twoSum_01(nums,target):
     sorted_id = sorted(range(len(nums)), key=lambda k: nums[k])
     head = 0
@@ -19,5 +42,7 @@ def twoSum_02(nums, target):
         if temp in hashset:
             return [hashset[temp],i]
         hashset[nums[i]] = i
+    return []
+
             
 print(twoSum_02([3,2,4],6))
