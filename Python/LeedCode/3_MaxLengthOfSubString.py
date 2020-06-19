28103f546ecc05bfeffdb02832d1298d1d415b33
Xiaohai.lin <tbfungeek@163.com>
2020-06-19 21:59:42 +0800

    无重复子串添加题目和标准答案

--- Python/LeedCode/3_MaxLengthOfSubString.py
+++ Python/LeedCode/3_MaxLengthOfSubString.py
@@ -1,3 +1,24 @@
+'''
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+'''
 def lengthOfLongestSubstring(s):
     """
     :type s: str
@@ -33,3 +54,23 @@ def lengthOfLongestSubstring(s):
     return max_length
 
 print(lengthOfLongestSubstring("HelloWorld"))
+
+
+'''
+性能最高的解法
+'''
+def lengthOfLongestSubstring_best(s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        st = {}
+        i ,ans = 0,0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]],i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+        return ans;
+
+print(lengthOfLongestSubstring_best("HelloWorld"))
