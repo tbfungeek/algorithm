8c925a29faf8aa3d012f460c8fae2d9e92a5c032
Xiaohai.lin <tbfungeek@163.com>
2020-06-19 21:53:04 +0800

    最大不重复子串题解

--- Python/LeedCode/3_MaxLengthOfSubString.py
+++ Python/LeedCode/3_MaxLengthOfSubString.py
@@ -3,22 +3,33 @@ def lengthOfLongestSubstring(s):
     :type s: str
     :rtype: int
     """
+
     charSet = set()
     count = 0
     max_length = 0
+    substring_start_index = 0
     for i in range(len(s)):
         charSet.add(s[i])
         count += 1
+        #如果发现count大于集合内的数量，说明有重复字符
         if count > len(charSet):
+
+            #charSet记录的是目前最大的长度
             if len(charSet) > max_length:
                 max_length = len(charSet)
-            charSet.clear()
-            charSet.add(s[i])
-            count = 1
 
+            #找到当前时刻最长的不重复的字符
+            index  = s.find(s[i], substring_start_index)
+            #将这次查找的在下一次搜索中剔除
+            substring_start_index = index+1
+            #重置集合及count数值
+            charSet = set(s[index + 1:i + 1])
+            count = len(charSet)
+            
+    #遍历结束后触发一次
     if max_length < len(charSet):
         max_length = len(charSet)
-        
-    print(max_length)
 
-lengthOfLongestSubstring("")
+    return max_length
+
+print(lengthOfLongestSubstring("HelloWorld"))
